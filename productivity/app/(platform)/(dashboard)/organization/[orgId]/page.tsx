14c967b21d1c716c52e130883f51ecbd30a2b647
Om Prakash Sahoo <omprakashsahoo1234@gmail.com>
2024-07-31 00:41:09 +0530

    creating actions for different querying activities to the database

--- productivity/app/(platform)/(dashboard)/organization/[orgId]/page.tsx
+++ productivity/app/(platform)/(dashboard)/organization/[orgId]/page.tsx
@@ -1,17 +1,17 @@
 // import { ClerkProvider, OrganizationSwitcher } from "@clerk/nextjs"
-import { auth } from "@clerk/nextjs/server"
+// import { auth } from "@clerk/nextjs/server"
 
 
 
 export default function OrganizationPage () {
   
-  const organizationId = auth().orgId;
+  // const organizationId = auth().orgId;
 
 
   return (
 
     <>
-      <div>Organization Id : {organizationId}</div>
+      <div>Welcome to Workspaces</div>
     </>
 
   )
--- productivity/lib/db.ts
+++ productivity/lib/db.ts
@@ -4,6 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
+// next js uses hot reload and db initializes lot more time so to avoid that error 
 export  const db = global.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db
