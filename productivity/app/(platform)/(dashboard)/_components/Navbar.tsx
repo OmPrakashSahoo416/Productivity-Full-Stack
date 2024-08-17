6bc2e089b9a9805e1cc28abb0a2e9790551b05a7
Om Prakash Sahoo <omprakashsahoo1234@gmail.com>
2024-08-17 16:18:10 +0530

    mobile sidebar is implemented

--- productivity/app/(platform)/(dashboard)/_components/Navbar.tsx
+++ productivity/app/(platform)/(dashboard)/_components/Navbar.tsx
@@ -4,6 +4,7 @@ import { UserButton, ClerkProvider, OrganizationSwitcher } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { auth } from "@clerk/nextjs/server";
+
 import NewBoardDialog from "../organization/[orgId]/_components/newBoardDialog";
 // import { toast } from "@/components/ui/use-toast";
 
@@ -14,16 +15,18 @@ export default function Navbar () {
   return (
 
     <>
-    <div className=" sticky z-[100] top-0 bg-slate-50 py-2 px-10 drop-shadow-sm border w-full justify-between  flex items-center">
+    <div className=" sticky z-[100] top-0 bg-slate-50 py-2 px-10 drop-shadow-sm border min-w-full w-fit justify-between  flex items-center">
 
       
       
       <div className="navLeft flex space-x-4 items-center">
       <Logo></Logo>
+     
       <NewBoardDialog>
       <div role="button" 
-       className="text-[12px]  bg-rose-600 hover:bg-rose-800 text-slate-100 rounded-sm font-medium px-3 py-2 hidden md:block">+ Create</div>
-      <div role="button"  className="md:hidden bg-rose-600 hover:bg-rose-800 text-slate-100  rounded-full w-9 h-9 flex justify-center items-center"><Plus ></Plus></div>
+       className="text-[12px]  bg-rose-600  hover:bg-rose-800 text-slate-100 rounded-sm font-medium px-3 py-2 hidden md:block">+ Create</div>
+      <div role="button"  className="md:hidden bg-rose-600 hover:bg-rose-800 text-slate-100  rounded-full w-6 h-6 flex justify-center items-center font-bold"><Plus className="h-4 font-bold w-4"></Plus></div>
+      
       </NewBoardDialog>
 
       </div>
--- productivity/app/(platform)/(dashboard)/organization/layout.tsx
+++ productivity/app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,17 +1,29 @@
-import { ReactNode } from "react"
+"use client"
+import { ReactNode, useState } from "react"
 import SideBar from "../_components/SideBar"
+import { ArrowRight } from "lucide-react"
 
 
 
 function OrganizationLayout ({children}: {children:ReactNode}) {
 
+  const [mobileSidebar, setMobileSidebar] = useState(false)
+
 
   return (
   <>
+  <div role="button" onClick={() =>setMobileSidebar(!mobileSidebar)} className="absolute peer z-[200] w-10 h-6  bg-slate-200 text-rose-600   flex md:hidden justify-end left-0 rounded-r-full"> <ArrowRight className="h-6 w-6" /> </div>
+  {mobileSidebar && (
+    <div className={`sideBar absolute z-[100]   pt-10 rounded-r-md bg-slate-200 h-full w-60 `}>
+      <SideBar></SideBar>
+    </div>
+  )}
+  
   
   <div className="organizationLayout mt-20 max-w-md space-x-10 flex mx-auto lg:mx-20 md:mx-10  lg:max-w-screen-lg">
     
 
+    
     <div className="sideBar lg:ml-20 ml-10  shrink-0 md:block hidden w-60">
           <SideBar></SideBar>
         </div>
