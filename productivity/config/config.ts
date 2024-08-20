6a97cebdd23ac4c5077bf7b1a0d7fb56acd22e12
Om Prakash Sahoo <omprakashsahoo1234@gmail.com>
2024-08-20 22:18:45 +0530

    Title changes

--- productivity/config/config.ts
+++ productivity/config/config.ts
@@ -1,6 +1,6 @@
 
 
 export const ConfigObject = {
-  name:"Trello",
+  name:"TaskWeave",
   description:"Manage your organization with ease"
 };
--- productivity/lib/db.ts
+++ productivity/lib/db.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '@/generated/prisma-client'
+// import { PrismaClient } from '@/generated/prisma-client'
 
-// import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
 
 declare global {
   var prisma: PrismaClient | undefined
